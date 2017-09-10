f45692144d1c94076f3c871a7fc048ca3d9b7825
Alex <alex.milkis@kaltura.com>
2017-09-10 23:37:01 +0300

    component changes

--- components/TopNavbar.js
+++ components/TopNavbar.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+
+menuItems = [
+    { url: "htt", name: },
+    {}
+]
+
 export default class TopNavbar extends React.Component {
 
     render() {
