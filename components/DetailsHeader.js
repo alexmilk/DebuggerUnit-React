5f66b2ced9c3f16ac737e52354c838c39a8473ed
Alex <alex.milkis@kaltura.com>
2017-09-24 16:57:26 +0300

    Added details header component

---
+++ components/DetailsHeader.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+export default class detailsHeader extends React.Component {
+
+    render() {
+        return (
+         <div></div>
+    );
+    }
+    }
+
+    ReactDOM.render(<Details />, document.getElementById('detailsHeader'));
