0eaacd84e3f4a52b5b9a7e3d2189441d64540deb
Alex <alex.milkis@kaltura.com>
2017-09-24 16:58:22 +0300

    Added footer component

---
+++ components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+export default class Footer extends React.Component {
+
+    render() {
+        return (
+         <div></div>
+    );
+    }
+    }
+
+    ReactDOM.render(<Footer />, document.getElementById('footer'));
